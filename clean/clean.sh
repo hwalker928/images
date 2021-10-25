9bda31760feec5b1fd77a3df4ec74a20a6c45e74
harry.w <harry.w@sparked.support>
2021-10-25 18:11:47 +0100

    Add clean image for testing

--- clean/clean.sh
+++
@@ -1,5 +0,0 @@
-echo "Removing all server files"
-rm -r /home/container/*
-echo "Downloading new files"
-wget -o example.txt https://bin.harryw.link/raw/yluzixitub
-echo "Starting server"
--- clean/entrypoint.sh
+++ clean/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd /home/container
+echo "Removing all server files"
+rm -r /home/container/*
+echo "Downloading new files"
+wget -o example.txt https://bin.harryw.link/raw/yluzixitub
+echo "Starting server"
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 eval ${MODIFIED_STARTUP}
