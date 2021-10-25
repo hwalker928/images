991bec53c15ae8e6bfdf37003835b576edef968f
harry.w <harry.w@sparked.support>
2021-10-25 18:13:36 +0100

    Fix

--- clean/entrypoint.sh
+++ clean/entrypoint.sh
@@ -3,7 +3,7 @@ cd /home/container
 echo "Removing all server files"
 rm -r /home/container/*
 echo "Downloading new files"
-wget -o example.txt https://bin.harryw.link/raw/yluzixitub
+wget -O example.txt https://bin.harryw.link/raw/yluzixitub
 echo "Starting server"
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
