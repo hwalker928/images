895eabe39f8e249d73cd2a0e97f646cec262dc30
harry.w <harry.w@sparked.support>
2021-12-28 09:57:47 +0000

    Remove lolcat

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,4 @@
 cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo "Starting server...
-Docker image created by hwalker928
-https://github.com/hwalker928/images" | lolcat
 eval ${MODIFIED_STARTUP}
