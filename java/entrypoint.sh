71c7d6c7ef843ccd325202fb3d0978ed93136f1d
harry.w <harry.w@sparked.support>
2022-01-12 17:14:24 +0000

    Start new java image

---
+++ java/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/container
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+MODIFIED_STARTUP=${$MODIFIED_STARTUP/java/java8}
+java -version
+eval ${MODIFIED_STARTUP}
