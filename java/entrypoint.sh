e3a4a2b67626fbf26735e4b151ad6638774cf23f
Harry W <harry@harryw.link>
2022-01-13 14:04:55 +0000

    Trigger rebuild

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -3,6 +3,6 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 MODIFIED_STARTUP=${$MODIFIED_STARTUP/java/java8}
-echo "Using Java 8"
+echo "Using Java 8."
 java8 -version
 eval ${MODIFIED_STARTUP}
