63a85da1bfedbbd3931776cf0a418adfeb6a8488
Harry W <harry@harryw.link>
2022-05-06 19:29:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-eval ${MODIFIED_STARTUP} # Run startup command
+eval ${MODIFIED_STARTUP}
